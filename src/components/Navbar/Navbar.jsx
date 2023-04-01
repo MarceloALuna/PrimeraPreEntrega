7341867acfd2193a35a03d468515b0cce5b31b49
MarceloALuna <marceloaluna@gmail.com>
2023-04-01 15:46:40 -0300

    correccion de errores

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="VIVERO">VIVERO</a>
+                    <a className="navbar-brand"href="VIVERO">Vivero Doña Luna</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" />
                     </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
