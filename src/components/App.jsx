1d115f1755502073bc5669dd1c1e31d31641d258
MarceloALuna <marceloaluna@gmail.com>
2023-04-22 20:37:04 -0300

    Segunda pre-entrega

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
-import { Navbar } from './Navbar/Navbar'
-import { ItemListContainer } from './ItemListContainer/ItemListContainer.jsx'
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Components 
+import { Navbar } from './Navbar/Navbar';
+import { ItemListContainer } from './ItemListContainer/ItemListContainer';
+import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer';
 export const App = () => {
   return (
-    <div id="divGeneral" >
-      <Navbar />
-      <ItemListContainer greeting={"Productos"} />
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer />} />
+          <Route path='/category/:category' element={<ItemListContainer />} />
+          <Route path='/product/:id' element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,18 @@
-import { CartWidget } from "../CartWidget/CartWidget"
+import { Categorias } from "./Categorias/Categorias";
+import { CartWidget } from "../CartWidget/CartWidget";
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand"href="VIVERO">Vivero Doña Luna</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" />
-                    </button>
+            <div className="container-fluid">
+                <h1 className="navbar-brand">Vivero Doña Luna</h1>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="Suculentas">Suculentas</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="Cactus">Cactus</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="Herramientas">Herramientas</a>
-                    </li>
-                </ul>
+                    <Categorias />
+                </div >
                 <div className="d-flex">
-                    <CartWidget cantCarrito={6} className="btn btn-outline-success"/>
+                <CartWidget cantCarrito={0} className="btn btn-outline-success"/>
+                </div>
             </div>
-        </div>
-    </div>
-</nav>
-   )
+        </nav>
+
+    );
 }
